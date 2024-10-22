740d71b17a14f7e769099259673137f0da478004
Andyka Putra Permana <andykapermana.com>
2024-10-22 19:26:26 +0700

    day #4

---
+++ 4-smash.js
@@ -0,0 +1,24 @@
+// DYY CODING
+// Sentence Smash
+// 4/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+// function smash(words) {
+//   return words.join(" ");
+// }
+
+// const smash = (words) => words.join(" ");
+
+// function smash(words) {
+//   let result = "";
+//   for (let i = 0; i < words.length; i++) {
+//     result += words[i];
+//     if (i != words.length - 1) result += " ";
+//   }
+
+//   return result;
+// }
+
+console.log(smash(["hello", "world"]));
+console.log(smash(["hello", "amazing", "world"]));
+console.log(smash(["this", "is", "a", "really", "long", "sentence"]));
