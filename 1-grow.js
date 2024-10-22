1dc3987dea012aee751cf9ea8425154f628ae558
Andyka Putra Permana <andykapermana.com>
2024-10-22 14:55:30 +0700

    day 1/366

---
+++ 1-grow.js
@@ -0,0 +1,27 @@
+// DYY CODING
+// Reduce But Grow
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result = result * x[i];
+//   }
+//   return result;
+// }
+
+// function grow(x) {
+//   const result = x.reduce((acc, curr) => acc * curr, 1);
+//   return result;
+// }
+
+// function grow(x) {
+//   return x.reduce((acc, curr) => acc * curr, 1);
+// }
+
+// const grow = (x) => x.reduce((acc, curr) => acc * curr * 1);
+
+console.log(grow([1, 2, 3]));
+console.log(grow([4, 1, 1, 1, 4]));
+console.log(grow([2, 2, 2, 2, 2, 2]));
